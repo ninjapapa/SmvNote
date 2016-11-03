7343382db4a66e307ed65e858635b3144e7d154d
ninjapapa <bzhangusc@live.com>
2016-11-03 10:37:08 -0700

    init commit

---
+++ conf/shell_init.scala
@@ -0,0 +1,2 @@
+
+import org.tresamigos.smvnote._
---
+++ src/main/scala/org/tresamigos/smvnote/stage2/input/InputFilesS2.scala
@@ -0,0 +1,7 @@
+package org.tresamigos.smvnote.stage2.input
+
+import org.tresamigos.smv._
+
+import org.tresamigos.smvnote.stage1._
+
+object EmploymentStateLink extends SmvModuleLink(EmploymentByState)
